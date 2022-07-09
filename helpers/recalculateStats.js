8d4ccb544306b85e68a3a7b5751036628a2e5a2f
Gabriel <gabriel@mozillafoundation.org>
2022-07-09 10:25:36 -0400

    Update delta stats script

--- helpers/recalculateStats.js
+++ helpers/recalculateStats.js
@@ -4,12 +4,23 @@ const args = process.argv.slice(2);
 const main = (filePath) => {
   const stats = JSON.parse(fs.readFileSync(filePath, "utf-8"));
   const data = stats.locales;
-
+  const calc = {
+    validDurationSecs: 0,
+    totalHrs: 0,
+  };
   let avg = 0;
   Object.keys(data).map((s) => {
-    avg += +data[s].validDurationSecs;
+    calc.validDurationSecs += +data[s].validDurationSecs;
+    calc.totalHrs += +data[s].totalHrs;
   });
-  console.log(avg);
+
+  let a = Object.keys(data).map((s, d) => {
+    return +data[s].validDurationSecs;
+  });
+  console.log(a.sort((a, b) => a - b));
+  console.log("Total validDurationSecs: ", calc.validDurationSecs);
+  console.log("Total validDurationSecs: ", calc.validDurationSecs);
+  console.log("Total Hours: ", calc.totalHrs);
 };
 
 main(args[0]);
