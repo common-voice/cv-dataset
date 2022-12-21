cfd989af9b7935ac62adb300d01b58caa777d7fa
Dmitrij Feller <dmitrij@mozillafoundation.org>
2022-12-21 15:31:53 +0100

    Fix totals in the delta stats

--- helpers/recalculateStats.js
+++ helpers/recalculateStats.js
@@ -5,21 +5,25 @@ const main = (filePath) => {
   const stats = JSON.parse(fs.readFileSync(filePath, "utf-8"));
   const data = stats.locales;
   const calc = {
-    validDurationSecs: 0,
+    totalDuration: 0,
+    totalValidDurationSecs: 0,
     totalHrs: 0,
+    totalValidHrs: 0
+
   };
   let avg = 0;
   Object.keys(data).map((s) => {
-    calc.validDurationSecs += +data[s].validDurationSecs;
+    calc.totalValidDurationSecs += +data[s].validDurationSecs;
+    calc.totalDuration += +data[s].duration;
     calc.totalHrs += +data[s].totalHrs;
+    calc.totalValidHrs += +data[s].validHrs;
   });
 
   let a = Object.keys(data).map((s, d) => {
     return +data[s].validDurationSecs;
   });
   console.log(a.sort((a, b) => a - b));
-  console.log("Total validDurationSecs: ", calc.validDurationSecs);
-  console.log("Total Hours: ", calc.totalHrs);
+  console.log(JSON.stringify(calc));
 };
 
 main(args[0]);
