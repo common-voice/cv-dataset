c44f501eed5b486404692296264dcccb158cd163
moz-bozden <bulent@mozillafoundation.org>
2026-02-16 09:32:55 +0300

    Update scripts to handle multiple dataset types, also changing invocation format

--- helpers/createStats.js
+++ helpers/createStats.js
@@ -2,12 +2,36 @@ const fs = require("fs/promises");
 const path = require("path");
 const process = require("node:process");
 
+const DATASET_TYPES = [
+  "scripted-speech",
+  "spontaneous-speech",
+  "code-switching",
+];
+const READY_TYPES = ["scripted-speech"];
+
 const getLocaleFromFilename = (filename) =>
   filename.split(".json")[0].split("_")[1];
 
-const main = async () => {
-  const args = process.argv;
-  const statsFolder = args[2];
+const buildPath = (datasetType, folderName) => {
+  return path.join(__dirname, "..", "datasets", datasetType, folderName);
+};
+
+const showUsage = () => {
+  console.log(
+    "\nUsage: node helpers/createStats.js <dataset-type> <stats-folder>",
+  );
+  console.log("\nExample:");
+  console.log("  node helpers/createStats.js scripted-speech stats-folder");
+  console.log("\nDataset Types:");
+  console.log("  Ready: " + READY_TYPES.join(", "));
+  console.log(
+    "  Upcoming: " +
+      DATASET_TYPES.filter((t) => !READY_TYPES.includes(t)).join(", "),
+  );
+  console.log();
+};
+
+const scriptedSpeech = async (statsFolder) => {
   const stats = await fs.readdir(statsFolder);
 
   const completeStats = { locales: {} };
@@ -58,4 +82,37 @@ const main = async () => {
   console.log(JSON.stringify(result));
 };
 
-main();
+const main = async () => {
+  const args = process.argv;
+  const datasetType = args[2];
+  const statsFolder = args[3];
+
+  showUsage();
+
+  if (!datasetType || !statsFolder) {
+    throw new Error("Must provide dataset type and stats folder");
+  }
+
+  if (!DATASET_TYPES.includes(datasetType)) {
+    throw new Error(`"${datasetType}" is not a valid dataset type`);
+  }
+
+  if (!READY_TYPES.includes(datasetType)) {
+    throw new Error(`Dataset type "${datasetType}" is not ready yet`);
+  }
+
+  const folderPath = buildPath(datasetType, statsFolder);
+
+  switch (datasetType) {
+    case "scripted-speech":
+      await scriptedSpeech(folderPath);
+      break;
+    default:
+      throw new Error(`Dataset type "${datasetType}" is not ready yet`);
+  }
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
